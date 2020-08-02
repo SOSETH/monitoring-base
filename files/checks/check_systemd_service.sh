e1db3ee8b7fb41ddcc776fd07110042991f91fe4
niklassc <niklas@schmatloch.de>
2020-08-03 00:05:22 +0200

    small changes, exit codes as nagios standard

--- files/checks/check_systemd_service.sh
+++ files/checks/check_systemd_service.sh
@@ -1,12 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
-[ -n "$2" ] || exit 1;
+[ -n "$2" ] || exit 3;
 
 is_active=$(systemctl is-active "$2");
-echo "$2 is $is_active on $(hostname)"
 
 if [ "$is_active" == active ]; then
+	echo "SYSTEMD OK: $2 is $is_active on $(hostname)"
 	exit 0;
 else
-	exit 1;
+	echo "SYSTEMD CRITICAL: $2 is $is_active on $(hostname)"
+	exit 2;
 fi
